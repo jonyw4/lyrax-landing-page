7c366d01abcb19c2d8f597444688145e0bdcd1e6
Jonathan Célio <jonathan.clio@hotmail.com>
2021-09-15 19:23:09 -0300

    feat: styling partner info component

--- src/components/PartnerInfo/PartnerInfo.component.tsx
+++ src/components/PartnerInfo/PartnerInfo.component.tsx
@@ -4,21 +4,26 @@ export function PartnerInfo({
   partner: { id, name, title, email, socialMediaPages },
 }: PartnerInfoProps) {
   return (
-    <article>
-      <h3>{name}</h3>
-      <span>{title}</span>
-      
-      <a href={`mailto:${email}`}>{email}</a>
+    <article className="mb-12 text-center">
+      <h3 className="text-3xl text-gray-900 font-semibold">{name}</h3>
+      <h4 className="text-1xl text-gray-900 font-semibold">{title}</h4>
 
-      {socialMediaPages.map((page) => (
-        <a
-          key={page.url}
-          href={page.url}
-          data-test={`partner-${id}-social-media-${page.name.toLowerCase()}`}
-        >
-          {page.name}
-        </a>
-      ))}
+      <ul className="flex justify-center divide-x">
+        <li className="text-sm px-2 uppercase text-gray-500">
+          <a href={`mailto:${email}`}>E-mail</a>
+        </li>
+
+        {socialMediaPages.map((page) => (
+          <li key={page.url} className="text-sm px-2 uppercase text-gray-500">
+            <a
+              href={page.url}
+              data-test={`partner-${id}-social-media-${page.name.toLowerCase()}`}
+            >
+              {page.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </article>
   );
 }
