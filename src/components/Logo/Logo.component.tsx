4791f2330eb7afe6b94462f228385096080e21c5
Jonathan Célio <jonathan.clio@hotmail.com>
2021-09-15 19:23:27 -0300

    feat: add logo component

---
+++ src/components/Logo/Logo.component.tsx
@@ -0,0 +1,11 @@
+export function Logo(){
+  return (
+    <img
+      src="https://lyrax.com.br/img/logo.svg"
+      alt="Lyrax"
+      title="Lyrax"
+      data-test="company-info"
+      width="80"
+    />
+  );
+}
