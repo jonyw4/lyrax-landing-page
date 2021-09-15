0f4fc1114d317749237ff9d447b5f8c6357d162f
Jonathan Célio <jonathan.clio@hotmail.com>
2021-09-15 19:23:33 -0300

    feat: styling feature list component

--- src/components/FeatureList/FeatureList.component.tsx
+++ src/components/FeatureList/FeatureList.component.tsx
@@ -3,7 +3,7 @@ import { FeatureListProps } from "./FeatureList.props";
 
 export function FeatureList({ list }: FeatureListProps) {
   return (
-    <section>
+    <section className="max-w-screen-lg mx-auto px-3 py-16">
       {list.map((feature, index) => (
         <Feature key={index} {...feature} />
       ))}
