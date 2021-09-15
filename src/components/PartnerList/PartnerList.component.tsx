92bf3f3a52d6ff2eb190a0f5430170f9e9f681d9
Jonathan Célio <jonathan.clio@hotmail.com>
2021-09-15 19:23:17 -0300

    feat: styling partner list component

--- src/components/PartnerList/PartnerList.component.tsx
+++ src/components/PartnerList/PartnerList.component.tsx
@@ -4,7 +4,7 @@ import { PartnerInfo } from '../PartnerInfo'
 export function PartnerList() {
   const partners = useCompanyPartners();
   return (
-    <section>
+    <section className="max-w-screen-lg mx-auto px-3 py-16">
       {partners.map((partner) => (
         <PartnerInfo partner={partner} key={partner.id} />
       ))}
