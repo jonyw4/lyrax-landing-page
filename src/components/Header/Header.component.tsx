7d14d03ac82f2ca0345a290b4d9ca5e01d253668
Jonathan Célio <jonathan.clio@hotmail.com>
2021-09-15 23:14:02 -0300

    refactor: header to use new button component

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,9 +1,9 @@
-import clsx from 'clsx';
 import { Logo } from '../Logo'
-import { useLocale } from '../_hooks'
+import { BtnLanguage  } from '../BtnLanguage';
+
 
 export function Header() {
-  const { setLocale, locale } = useLocale();
+
   return (
     <header className="max-w-screen-lg mx-auto px-3 py-6">
       <div className="flex flex-wrap justify-between items-center">
@@ -11,20 +11,8 @@ export function Header() {
           <Logo />
         </div>
         <div className="divide-x">
-          <button
-            onClick={() => setLocale("pt-BR")}
-            data-test="button-change-i18-pt-BR"
-            className={clsx("px-2", { "font-weigh-700": locale === "pt-BR" })}
-          >
-            PT
-          </button>
-          <button
-            onClick={() => setLocale("en")}
-            data-test="button-change-i18-en"
-            className={clsx("px-2", { "font-weigh-700": locale === "en" })}
-          >
-            EN
-          </button>
+          <BtnLanguage language="pt-BR" />
+          <BtnLanguage language="en" />
         </div>
       </div>
     </header>
