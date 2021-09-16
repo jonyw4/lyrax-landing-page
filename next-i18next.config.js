5edcd834deff27f07ce610e3dd633f4361c99268
Jonathan Célio <jonathan.clio@hotmail.com>
2021-09-15 23:39:03 -0300

    refactor: add domains config i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,15 @@ module.exports = {
   i18n: {
     defaultLocale: "pt-BR",
     locales: ["en", "pt-BR"],
+    domains: [
+      {
+        domain: "lyrax.com.br",
+        defaultLocale: "pt-BR",
+      },
+      {
+        domain: "lyrax.systems",
+        defaultLocale: "en",
+      },
+    ],
   },
 };
